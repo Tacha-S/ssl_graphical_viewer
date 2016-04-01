9f2bd72466b8c8d9ae366870fa9c07f47a092034
tacha-S <tacchan.mello.ioiq@gmial.com>
2016-04-01 14:58:22 +0900

    Add to show main window files.

---
+++ main.js
@@ -0,0 +1,22 @@
+var app = require('app');
+var browser_window = require('browser-window');
+
+var main_window = null;
+
+//crash-report
+require('crash-reporter').start();
+
+//quit, if all window closed
+app.on('window-all-closed', function(){ app.quit(); });
+
+
+app.on('ready', function(){
+  //create main window
+  main_window = new browser_window({ width: 1200, height: 800 });
+
+  //load html
+  main_window.loadURL('file://' + __dirname + '/index.html');
+
+  //if clicked close button, close main window.
+  main_window.on('closed', function(){ main_window = null; });
+})
